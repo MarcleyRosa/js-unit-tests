9a2bd99f50390d0c7083e107d7a9106ad2a7aab3
Marcley <marcleyrosa@gmail.com>
2022-06-21 11:50:51 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,13 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
